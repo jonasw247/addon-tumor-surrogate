a60083d0eefbf418edac0ec632682b188795435c
jonasw247 <jonas.weidner@outlook.de>
2024-04-03 13:24:07 +0200

    minor

--- tumor_surrogate_pytorch/optimize_input.py
+++ tumor_surrogate_pytorch/optimize_input.py
@@ -22,15 +22,16 @@ model = TumorSurrogate(widths=[128, 128, 128, 128], n_cells=[5, 5, 5, 4], stride
 #os.environ['CUDA_VISIBLE_DEVICES'] = "6"
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = model.to(device=device)
-# chose from epoch 17 
+# %% load test data:
+# chose from epoch 17, 25000 was also good with dice. from daily-cherry-69
 model.load_state_dict(torch.load('/mnt/8tb_slot8/jonas/workingDirDatasets/tumor-surrogate-model-states/daily-cherry-69/modelsWeights/epoch17.pth'))
 
-# %% load test data:
-start = 20005 
+start = 20010
 number_of_samples = 1
 stop = start + number_of_samples
 dataset = MyDataset(start=start, stop=stop)
 plotforStep = 10
+fixOrigin = False
 
 data_loader = torch.utils.data.DataLoader(dataset, batch_size=number_of_samples,
                                             num_workers=16, pin_memory=True, shuffle=False)
@@ -38,9 +39,10 @@ data_loader = torch.utils.data.DataLoader(dataset, batch_size=number_of_samples,
 nBatch = len(data_loader)
 myOptim = torch.optim.Adam
 optimizationSteps = 100
+learningRate = 0.01#0.01
 
 
-configForWandb = { 'nBatch': nBatch, 'number_of_samples': number_of_samples, 'start': start, 'stop': stop, 'optimizationSteps': optimizationSteps, "myOptim": myOptim.__name__}
+configForWandb = { 'nBatch': nBatch, 'number_of_samples': number_of_samples, 'start': start, 'stop': stop, 'optimizationSteps': optimizationSteps, "myOptim": myOptim.__name__, "learningRate": learningRate, "model": model.__class__.__name__, "fixOrigin": fixOrigin, "plotforStep": plotforStep}
 wandb.init(project="optimizeInput", config=configForWandb)
 wandb.watch(model)
 
@@ -63,33 +65,44 @@ for i0, (input_tissue, parameters_ground_truth, output_ground_truth) in enumerat
     parameters = parameters.to(device)
     parameters.requires_grad = True
 
-    optimizer = myOptim([parameters], lr=0.01)
+    optimizer = myOptim([parameters], lr=learningRate)
 
     for optimStep in range(optimizationSteps):
         optimizer.zero_grad()
         prediction = model(input_tissue, parameters)
         mask = input_tissue[:, 0].unsqueeze(1)  > 0.001
         prediction_masked = prediction.to(device) * mask.to(device)
-        #loss = F.mse_loss(prediction_masked, output_ground_truth.to(device))
-        loss = F.l1_loss(prediction_masked, output_ground_truth.to(device))
+        loss = F.mse_loss(prediction_masked, output_ground_truth.to(device))
+        #loss = F.l2_loss(prediction_masked, output_ground_truth.to(device))
         grad_parameters, = autograd.grad(loss, parameters, retain_graph=True)
         
         loss.backward()
+
+        # Manually zero out gradients for fixed entries
+        # dont allow to change the origin, but fix it at COM
+        if fixOrigin:
+            with torch.no_grad():
+                parameters.grad[:,0] = 0
+                parameters.grad[:,1] = 0
+                parameters.grad[:,2] = 0
         optimizer.step()
+
                
         logDict = {}
         logDict["_loss"] = loss.item()
         logDict["_grad_parameters_mean"] = grad_parameters.mean().item()
         #log each grad_parameter
-        parameterDiff = torch.abs(parameters - parameters_ground_truth.to(device))
+        parameterDiff = parameters - parameters_ground_truth.to(device)
         labels = ["x", "y", "z", "muD", "muRho"]
         for j in range(5):
             logDict["grad_parameters_" + labels[j]] = grad_parameters[0,j].item()
             logDict["parameters_" + labels[j]] = parameters[0,j].item()
             logDict["parameters_difference_" + labels[j]] = parameterDiff[0,j].item()
+        #log mean parameter difference
+        logDict["_parameters_difference_mean"] = parameterDiff.abs().mean().item()
             
     
-        for threshold in np.linspace(0.1, 0.9, 10):
+        for threshold in np.linspace(0.1, 0.9, 9):
             dice = utils.compute_dice_score(prediction_masked, output_ground_truth.to(device), threshold=threshold)
             stringTh = "dice_" + str(round(threshold, 2))
             logDict[stringTh] = dice.item()
@@ -146,4 +159,28 @@ else:
 
 
 # %
+# %%
+
+# Printing only the weights
+for name, param in model.named_parameters():
+    if "weight" in name:  # Check if 'weight' is in parameter name
+        print(f"Layer: {name} | Size: {param.size()} | Values : \n{param.data} \n")
+
+# %%
+def check_weights(model1, model2):
+    models_identical = True
+    for (name1, param1), (name2, param2) in zip(model1.named_parameters(), model2.named_parameters()):
+        if name1 != name2 or not torch.equal(param1, param2):
+            print(f"Weights differ at layer: {name1}")
+            models_identical = False
+            break
+    if models_identical:
+        print("All model weights are identical.")
+    else:
+        print("Models have different weights.")
+
+model2 = TumorSurrogate(widths=[128, 128, 128, 128], n_cells=[5, 5, 5, 4], strides=[2, 2, 2, 1])
+model2 = model2.to(device=device)
+model2.load_state_dict(torch.load('/mnt/8tb_slot8/jonas/workingDirDatasets/tumor-surrogate-model-states/daily-cherry-69/modelsWeights/epoch17.pth'))
+check_weights(model, model2)
 # %%

6cae26cd6040b6217cd6cf3a22dcf47447a65e66
jonasw247 <jonas.weidner@outlook.de>
2024-04-16 11:39:04 +0200

    plot

--- tumor_surrogate_pytorch/plotFigures.py
+++ tumor_surrogate_pytorch/plotFigures.py
@@ -2,12 +2,17 @@
 import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+
 
 # %%
 pat = "rec020_pre"
 z = 30
-#pat = "rec006_pre"
+pat = "rec006_pre"
+z = 32
+#pat = "rec034_pre"
 #z = 32
+
 path = "/mnt/8tb_slot8/jonas/workingDirDatasets/addon-tumor-surrogate-output/fixOrigin/realData/optimizeOutputPatients/"+pat+"/"
 
 prediction_masked = np.flip(nib.load(path + "prediction_masked.nii.gz").get_fdata(), axis=0)
@@ -22,12 +27,21 @@ plt.imshow(output_ground_truth[:,:,z]**0.5, cmap="Greens", alpha=0.7 * output_gr
 plt.savefig(outoutPath + "output_ground_truth.svg")
 # %%
 # %%
-plt.imshow(prediction_smooth[:,:,z], cmap="Greens", alpha=prediction_smooth[:,:,z] )
+start_color = '#C1F7CB'  
+#start_color = "#E1FBE6"
+#start_color = "#BEE0C4"
+end_color = "#094413"#'#086218'    
+custom_cmap = LinearSegmentedColormap.from_list('custom_blue_red', [start_color, end_color])
+
+plt.imshow(prediction_smooth[:,:,z], cmap=custom_cmap, alpha=0.8*(prediction_smooth[:,:,z] >0.01))
+plt.colorbar()
+
 plt.savefig(outoutPath + "prediction_smooth.svg")
 
 # %% difference
 toplt = np.abs(prediction_masked[:,:,z] - output_ground_truth[:,:,z])
 plt.imshow(toplt, cmap="Reds", alpha=toplt)
+plt.colorbar()
 
 
 # %%
